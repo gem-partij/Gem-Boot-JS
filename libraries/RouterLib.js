d35cb87416ce1e7107e3f28ea64d7a84a229bd7d
Angger P. Putro @(HP :-- Ubuntu) <anggerpputro@gmail.com>
2019-05-31 12:54:42 +0700

    add library to add crud route

---
+++ libraries/RouterLib.js
@@ -0,0 +1,29 @@
+const auth = require.main.require("./middleware/CheckAuthentication");
+
+class RouterLib {
+	constructor(router) {
+		this.router = router;
+	}
+
+	crud(path, controllerPath) {
+		const Controller = require.main.require(controllerPath);
+
+		this.router.get(path + "/", auth, (req, res) => {
+			new Controller().index(req, res);
+		});
+		this.router.get(path + "/:id", auth, (req, res) => {
+			new Controller().show(req, res);
+		});
+		this.router.post(path + "/", auth, (req, res) => {
+			new Controller().store(req, res);
+		});
+		this.router.patch(path + "/:id", auth, (req, res) => {
+			new Controller().update(req, res);
+		});
+		this.router.delete(path + "/:id", auth, (req, res) => {
+			new Controller().destroy(req, res);
+		});
+	}
+}
+
+module.exports = RouterLib;
--- routers/router.js
+++ routers/router.js
@@ -3,12 +3,17 @@ const debugRequest = require("debug")("app:request");
 const express = require("express");
 const router = express.Router();
 
+const RouterLib = require.main.require("./libraries/RouterLib");
+const GBRoute = new RouterLib(router);
+
 router.use((req, res, next) => {
 	debugRequest("Request comming in: " + req.method + " " + req.originalUrl);
 	next();
 });
 
 router.use("/auth", require("./modules/auth"));
-router.use("/api/user", require("./modules/user"));
+// router.use("/api/user", require("./modules/user"));
+
+GBRoute.crud("/api/user", "./controllers/UserController");
 
 module.exports = router;
