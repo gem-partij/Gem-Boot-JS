28a0476d3bbac13ac0c695f90e1aa1593aa2fb9b
Angger P. Putro @(HP :-- Ubuntu) <anggerpputro@gmail.com>
2019-06-24 13:04:59 +0700

    add config plugins rate limiter

--- config/plugins.js
+++ config/plugins.js
@@ -2,6 +2,13 @@ module.exports = {
 	morgan: {
 		enable: true
 	},
+	"rate-limiter": {
+		enable: false,
+		config: {
+			windowMs: 10 * 60 * 1000, // 10 minutes
+			max: 600 // limit each IP to 600 requests per windowMs
+		}
+	},
 	helmet: {
 		enable: true
 	},
